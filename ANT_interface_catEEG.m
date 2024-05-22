e07ff469a58ae46d6f8924423f78970c9b647560
mh105 <alexhe0218@gmail.com>
2024-05-21 21:55:36 -0700

    FF: update catEEG using EEGLAB pop_mergeset()

--- ANT_interface_catEEG.m
+++ ANT_interface_catEEG.m
@@ -7,7 +7,7 @@ function [EEG] = ANT_interface_catEEG(EEG, EEG_new)
 % long recordings. This function also corrects sample latencies of event
 % logs to make them accurate in the resultant master EEG structure. 
 %
-% Last edit: Alex He 08/30/2019
+% Last edit: Alex He 05/21/2024
 
 %%
 % Confirm that the sampling rates are identical and the same original files
@@ -15,21 +15,24 @@ assert(EEG.srate == EEG_new.srate, 'Different sampling rate, please check!')
 assert(strcmp(EEG.comments, EEG_new.comments), 'Different original file!')
 assert(size(EEG.data, 1) == size(EEG_new.data, 1), 'Different numbers of channels!')
 
-% Manually updating each field in the EEG structure to concatenate the
-% second segment encoded in EEG_new
-for i = 1:length(EEG_new.event)
-    EEG_new.event(i).latency = EEG_new.event(i).latency + EEG.pnts;
-    EEG.event(length(EEG.event)+1) = EEG_new.event(i);
-end
+% % Manually updating each field in the EEG structure to concatenate the
+% % second segment encoded in EEG_new
+% for i = 1:length(EEG_new.event)
+%     EEG_new.event(i).latency = EEG_new.event(i).latency + EEG.pnts;
+%     EEG.event(length(EEG.event)+1) = EEG_new.event(i);
+% end
+% 
+% EEG.pnts = EEG.pnts + EEG_new.pnts;
+% EEG.data = [EEG.data, EEG_new.data];
+% if EEG_new.xmin == 0
+%     EEG.times = [EEG.times, EEG_new.times + EEG.times(end) + 1/EEG.srate*1000];
+% else
+%     EEG.times = [EEG.times, EEG_new.times];
+% end
+% EEG.xmax = EEG_new.xmax;
 
-EEG.pnts = EEG.pnts + EEG_new.pnts;
-EEG.data = [EEG.data, EEG_new.data];
-if EEG_new.xmin == 0
-    EEG.times = [EEG.times, EEG_new.times + EEG.times(end) + 1/EEG.srate*1000];
-else
-    EEG.times = [EEG.times, EEG_new.times];
-end
-EEG.xmax = EEG_new.xmax;
+% Use EEGLAB function to merge the structures
+EEG = pop_mergeset(EEG, EEG_new);
 
 % Check if impedance measures should be incorporated
 if ~isempty(EEG_new.endimp)
